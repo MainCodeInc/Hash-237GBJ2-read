153fc428d4cd452dd88498cecf2d7e402e410c01
MainCode Inc <99421749+MainCodeInc@users.noreply.github.com>
2022-02-23 06:09:41 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+#This program spells out the user input!
+
+import time
+
+name = input("Enter some text: ")
+time.sleep(2)
+print("Compiling...")
+time.sleep(1)
+print("------------------------")
+time.sleep(2)
+
+for letter in name:
+    print(letter or x)
+
+#End of code!
